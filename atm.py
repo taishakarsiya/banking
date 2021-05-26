90d390e0f95a4a0b5b0359d0e41fe390523ce800
taishakarsiya <66948929+taishakarsiya@users.noreply.github.com>
2021-05-26 22:05:52 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,20 @@
+class Atm(object):
+    def __init__(CashWithdrawl, BalanceEnquiry=None):
+        self.CashWithdrawl = CashWithdrawl
+        self.BalanceEnquiry = BalanceEnquiry
+        self.gender = gender
+        self.level = level
+        self.grades = grades or {}
+
+    def setBalance(BalanceEnquiry):
+        self.BalanceEnquiry[course] = BalanceEnquiry
+
+     
+
+# Define some students
+john = Student("John", 35, "male", 6, {"balance": 20000})
+jane = Student("Jane", 32, "female", 6, {"balance":1.9})
+
+# Now we can get to the grades easily
+print(john.balanceA())
+print(jane.balance())
